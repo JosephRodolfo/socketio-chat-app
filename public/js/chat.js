3b8f27da42193633a81ae6513ecf4be0580bf91b
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-05-06 17:47:35 -0400

    initial commit

---
+++ public/js/chat.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+
+socket.on('message', (message) => {
+    console.log(message);
+})
+
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.message.value;
+  socket.emit("sendMessage", message);
+});
+
+
+
